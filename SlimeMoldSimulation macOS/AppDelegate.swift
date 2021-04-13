b8ded989fcb9e3187c9209e45f243ea2e35bad21
Nikola Bozhkov <bozhkov.nikola@gmail.com>
2021-04-13 03:49:59 +0300

    Initial Commit

---
+++ SlimeMoldSimulation macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  SlimeMoldSimulation macOS
+//
+//  Created by Nikola Bozhkov on 13.04.21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
